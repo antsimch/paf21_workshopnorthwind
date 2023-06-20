22536741b4a7770c476b5312c74f5fd3563b11eb
antsimch <anthonysimch@gmail.com>
2023-06-20 18:45:07 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/paf21_workshopnorthwind/Paf21WorkshopnorthwindApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf21_workshopnorthwind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paf21WorkshopnorthwindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Paf21WorkshopnorthwindApplication.class, args);
+	}
+
+}
